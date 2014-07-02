e97e9cb70f707ecf59afe82f94faa7b44d151070
shubham27goyal <shubham.goyal.ece12@iitbhu.ac.in>
2014-07-02 21:24:08 +0530

    removing bugs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var app = express();
 var routes = require('./routes/index');
 
 app.use(favicon());
